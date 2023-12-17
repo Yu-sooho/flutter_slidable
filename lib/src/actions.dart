aeb199a28e1073f8fce61f78773ad01e92c6affe
Yu-sooho <souho456@naver.com>
2023-12-17 17:03:15 +0900

    Feat: add iconSize

--- lib/src/actions.dart
+++ lib/src/actions.dart
@@ -98,8 +98,8 @@ class CustomSlidableAction extends StatelessWidget {
           style: OutlinedButton.styleFrom(
             padding: padding,
             backgroundColor: backgroundColor,
-            disabledForegroundColor: effectiveForegroundColor.withOpacity(0.38),
-            foregroundColor: effectiveForegroundColor,
+            primary: effectiveForegroundColor,
+            onSurface: effectiveForegroundColor,
             shape: RoundedRectangleBorder(
               borderRadius: borderRadius,
             ),
@@ -139,6 +139,7 @@ class SlidableAction extends StatelessWidget {
     this.icon,
     this.spacing = 4,
     this.label,
+    this.iconSize = 24,
     this.borderRadius = BorderRadius.zero,
     this.padding,
   })  : assert(flex > 0),
@@ -176,13 +177,18 @@ class SlidableAction extends StatelessWidget {
   /// Padding of the OutlinedButton
   final EdgeInsets? padding;
 
+  final double iconSize;
+
   @override
   Widget build(BuildContext context) {
     final children = <Widget>[];
 
     if (icon != null) {
       children.add(
-        Icon(icon),
+        Icon(
+          icon,
+          size: iconSize,
+        ),
       );
     }
 
